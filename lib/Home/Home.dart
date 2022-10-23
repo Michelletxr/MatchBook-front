0c807f5fdf4fe1eae17af8c9e41d7bed68461dba
Fernanda <nandagb23@gmail.com>
2022-10-23 20:14:20 -0300

    tela home adicionada

---
+++ lib/Home/Home.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          title: Text(
+            "Livros perto de você",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+          elevation: 0,
+        ),
+        body: CustomScrollView(
+          slivers: [
+            SliverPadding(
+              padding: EdgeInsets.all(8.0),
+              sliver: SliverAppBar(
+                pinned: true,
+                bottom: PreferredSize(
+                  preferredSize: const Size.fromHeight(0.0),
+                  child: Transform.translate(
+                    offset: const Offset(0, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Filtrar por",
+                          style: TextStyle(
+                            color: Colors.black,
+                          )
+                        ),   
+                        Row(children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          TextButton(
+                          child: Text(
+                            "Localização",
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                          style: TextButton.styleFrom(
+                            side: BorderSide(width: 1.0),
+                          ),
+                          onPressed: () {print('localização');}, 
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        TextButton(
+                          child: Text("Gênero",
+                          style: TextStyle(
+                            color: Colors.black,
+                          )
+                        ),
+                        style: TextButton.styleFrom(
+                            side: BorderSide(width: 1.0),
+                          ),
+                        onPressed: () {print('gênero');}, 
+                        ),
+                        ],)
+                      ]
+                    ),
+                  ),
+                ),
+                backgroundColor: Colors.white,
+              ),
+            ),
+            SliverGrid(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 150.0,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 0.7,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    alignment: Alignment.center,
+                    child: Image(image: AssetImage('imagens/capa.jpg'),)
+                  );
+                },
+                childCount: 100,
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+                onPressed: () {}, icon: Icon(Icons.home), color: Colors.grey),
+            IconButton(
+                onPressed: () {}, icon: Icon(Icons.search), color: Colors.grey),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.account_box_rounded),
+                color: Colors.grey)
+          ],
+        )
+        );
+  }
+}
+
