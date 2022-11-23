70b6aa5c40478fad6bd4acc7420a670a12de9820
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-11-22 22:54:07 -0300

    corrige importação tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import "package:match_book_front/Login/Login.dart";
+import 'package:match_book_front/Login/login.dart';
 
 void main() {
   runApp(MaterialApp(
