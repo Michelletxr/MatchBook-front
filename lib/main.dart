c76dad43f827dfcc81adab913b3e38afe114481d
Francisco Italo Silva do Nascimento <italo.silvan87@gmail.com>
2022-12-03 13:22:42 -0300

    Tela profile finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:match_book_front/Login/login.dart';
 import 'package:match_book_front/Register/register.dart';
-import 'package:match_book_front/Profile/profile.dart';
 
 void main() {
   // ignore: prefer_const_constructors
   runApp(MaterialApp(
     title: "Match book",
     // home: Login(),
-    home: Profile(
-      child: null,
-    ),
+    home: Login(),
   ));
 }
